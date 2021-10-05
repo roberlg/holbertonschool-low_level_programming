8d60737da1c360c0affe535efddf053461eacec0
roberlg <rlg2589@gmail.com>
2021-10-05 20:05:57 +0000

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print alphabet x10- prints 10 times the aphabet, in lowercase
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+		_putchar(c);
+	}
+	_putchar('\n');
+}
