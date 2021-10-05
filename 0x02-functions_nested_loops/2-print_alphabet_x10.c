62868a1ecc8142617444ffb162a0c969919b333d
roberlg <rlg2589@gmail.com>
2021-10-05 20:10:04 +0000

    alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,19 +8,16 @@
 void print_alphabet_x10(void)
 {
 	char c;
+	int i;
 
-	for (c = 'a' ; c <= 'z' ; c++)
+	for (i = '0' ; c <= '10' ; i++)
 	{
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
-		_putchar(c);
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+
+
 	}
 	_putchar('\n');
 }
