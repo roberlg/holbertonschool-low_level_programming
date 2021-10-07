3f8b99a4ab576a1781b995c6749fda8c239f4c83
roberlg <rlg2589@gmail.com>
2021-10-07 16:19:32 +0000

    print last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
- int _putchar(char c);	
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);	
  void print_alphabet(void);
  void print_alphabet_x10(void);
  int _islower(int c);
@@ -10,3 +13,4 @@
  void times_table(void);
  int add(int,int);
  void print_to_98(int n);
+#endif	
