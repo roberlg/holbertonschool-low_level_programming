2998c018848ec40e5324be4c89300b7f07fdd351
roberlg <rlg2589@gmail.com>
2021-10-08 04:10:14 +0000

    print most numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-#endif 
+void print_number(int);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
