09294b662d2d36d011ed492d4f3a011cc5080ff9
roberlg <rlg2589@gmail.com>
2021-10-08 03:59:42 +0000

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,9 @@
  **/
 void print_numbers(void)
 {
-	int i = 0;
+	char i;
 
-	while (i <= 9)
-	{
-		_putchar(i + '0');
-		++i;
-	}
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
 	_putchar('\n');
 }
