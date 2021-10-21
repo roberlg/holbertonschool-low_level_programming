c92e3df5d2bed23c1dc154dbc542ffe0cb462c7a
roberlg <rlg2589@gmail.com>
2021-10-21 02:40:43 +0000

    pow recursion

---
+++ 0x08-recursion/4-pow_recurison.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - find x^y using factorial
+ * @x: base number
+ * @y: power of
+ * Return: x^y, -1 if y is negative
+ **/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
