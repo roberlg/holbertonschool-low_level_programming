e27860e296325ec08c1ea1bf2a0549ddbe07172c
roberlg <rlg2589@gmail.com>
2021-10-21 21:23:21 +0000

    is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * prime_con - determine if n is a prime
+ * prime - determine if n is a prime
  * @n: test case
  * @i: recurring factors
  * Return: 1 if n is a prime, 0 if not
  **/
-int prime_con(int n, int i)
+int prime(int n, int i)
 {
 	if (n % i == 0)
 		return (0);
 
 	else if (n >= (i + 1) * (i * 1))
-		return (prime_con(n, i + 1));
+		return (prime(n, i + 1));
 
 	return (1);
 }
@@ -26,5 +26,5 @@ int is_prime_number(int n)
 	if (n < 2)
 		return (0);
 
-	return (prime_con(n, 2));
+	return (prime(n, 2));
 }
