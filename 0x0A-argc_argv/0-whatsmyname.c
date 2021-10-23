8f3137469fd2680bd584030edc14002409b3edbc
roberlg <rlg2589@gmail.com>
2021-10-23 02:24:56 +0000

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 /**
-*main -print its name
-*@argc: number of arguments. 
-*@argv: array pointer to the argument.
-*Return: 0
-*/
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 
+ **/
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc >= 0)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[0]);
-		}
-	}
+	argc--;
+	printf("%s\n", argv[0]);
 	return (0);
 }	
