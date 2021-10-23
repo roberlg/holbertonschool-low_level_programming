1ce59474f81d2cf8370aa7d7a794e6aa85d5cc8b
roberlg <rlg2589@gmail.com>
2021-10-23 02:33:27 +0000

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,12 @@
  * main - program prints its own name.
  * @argc: argument count
  * @argv: argument vectors
- * Return: 0 
- **/
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	argc--;
 	printf("%s\n", argv[0]);
 	return (0);
-}	
+
+}
