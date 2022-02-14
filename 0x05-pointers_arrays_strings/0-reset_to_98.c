48c2443d100907ace941b0872a9152cb8b274d1f
roberlg <rlg2589@gmail.com>
2022-02-14 17:20:45 +0000

    fixed 2-strlen

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,14 @@
 
 /**
  * reset_to_98 - set value of int to 98
+ *
  * @n: address of value to change
+ *
  * Description: resets *n to 98
+ *
  * Return: nothing
  **/
+
 void reset_to_98(int *n)
 {
 	*n = 98;
