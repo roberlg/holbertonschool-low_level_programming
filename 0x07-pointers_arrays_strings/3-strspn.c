2b51fe5058c87cbe521316686d6dfacc7d07f1e1
roberlg <rlg2589@gmail.com>
2022-02-23 15:20:58 +0000

    fixed 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,17 @@
 #include "main.h"
+
 /**
  * _strspn - get length of a prefix substring
+ *
  * @s: string
+ *
  * @accept: bytes being compared
+ *
  * Description: finds number of bytes required to find accept in s
+ *
  * Return: bytes required to find accept
  **/
+
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, exit;
